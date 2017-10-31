38b4b263728e4085f212e0ac3fd1011c7010c8fd
Suleman <sultarik@pdx.edu>
2017-10-31 14:55:33 -0700

    jkl

--- node/6-dns.js
+++ node/6-dns.js
@@ -8,3 +8,27 @@ var dns = require('dns'); // do not change this line
 // http://localhost:8080/sniklaus.com should return '216.239.36.21\n216.239.38.21\n216.239.32.21\n216.239.34.21' in plain text (addresses / order might be different)
 
 // http://localhost:8080/error should return 'error' in plain text
+
+var server = http.createServer(function (req, res){
+
+    if(req.url.indexOf('/') == 0) {res.write(200, {'Content-Type': 'text/plain'});}
+
+    dns.resolve4(decodeURI(req.url.substr(1)), function(err, address){
+
+        if(err){res.end('error');}
+
+        else{
+
+            var tempVar = '';
+
+            for(var i=0; i<address.length; i++){
+
+                tempVar += address[i];
+            }
+
+            res.end(tempVar);
+        }
+    });
+});
+
+server.listen(process.env.PORT || 8080);
