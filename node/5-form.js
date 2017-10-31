1b476fd88fee531ab28dd6b86511707e207cc8b5
Suleman <sultarik@pdx.edu>
2017-10-31 15:20:48 -0700

    jkl

--- node/5-form.js
+++ node/5-form.js
@@ -34,3 +34,70 @@ var querystring = require('querystring'); // do not change this line
 // [the server restarts and looses all messages]
 
 // http://localhost:8080/list should return '' in plain text
+
+var myArray = [];
+
+var server = http.createServer(function (req, res) {
+    var postData = "";
+    var parsedData = "";
+
+    var nameMessage = "";
+
+    if(req.url === '/') {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('you have accessed the ');
+        res.end('root');
+    }
+    else if (req.url === '/form') {
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<!DOCTYPE html><html><body><form action="/new" ' +
+                  'method="post"><input type="text" name="name"><input type="text" name="message">' +
+                  '<input type="submit" value="submit"></form></body></html>');
+
+        res.end();
+    }
+    else if(req.method == 'POST' && req.url === '/new')
+    {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+
+        req.on('data', function (data) {
+            postData += data;
+
+            if(postData.length > 1e6) req.end();
+        });
+
+        req.on('end', function () {
+
+            parsedData = querystring.parse(postData);
+            console.log(parsedData);
+
+            nameMessage += parsedData.name + ": " + parsedData.message;
+            console.log(nameMessage);
+            myArray.push(nameMessage);
+
+            res.end('thank you for your message');
+        });
+    }
+
+    else if(req.method == 'GET' && req.url === '/list')
+    {
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+
+        var messages = "";
+
+        for(var i =0; i < myArray.length; i++)
+        {
+            if(i !== (myArray.length-1)) messages += myArray[i] + '\n';
+
+            else messages += myArray[i];
+        }
+        res.end(messages);
+    }
+
+    else res.end();
+});
+
+server.listen(process.env.PORT || 8080);
