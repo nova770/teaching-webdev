d385165eae6b0fb11aa63be06e102a64d17153ab
Suleman <sultarik@pdx.edu>
2017-10-31 00:57:59 -0700

    jkl

--- node/1-lorem.js
+++ node/1-lorem.js
@@ -5,7 +5,7 @@ var http = require('http'); // do not change this line
 // any request should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
 var server = http.createServer(function(req, res){
     res.writeHead(200, {
-        'Content-Type': text/plain''
+        'Content-Type': 'text/plain'
     });
 
     res.write('<!DOCTYPE html><html><body>lorem ipsum</body></html>');
