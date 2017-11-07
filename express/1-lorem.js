1db9678d0fc34a0762675993d7f17326c90f604c
Suleman <sultarik@pdx.edu>
2017-11-07 13:24:25 -0800

    jkl

--- express/1-lorem.js
+++ express/1-lorem.js
@@ -3,3 +3,10 @@
 var express = require('express'); // do not change this line
 
 // http://localhost:8080/lorem should return '<!DOCTYPE html><html><body>lorem ipsum</body></html>' as html
+var server = express();
+
+server.get('/lorem', function(req, res){
+    res.send('<!DOCTYPE html><html><body>lorem ipsum</body></html>');
+});
+
+server.listen(process.env.PORT || 8080);
